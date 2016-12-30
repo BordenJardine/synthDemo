b97886c2c64a358b880a54608c685aa948b0c4eb
Matt J <bordenjardine@gmail.com>
2016-12-29 22:17:52 -0500

    big scope

--- envelope_demo.js
+++ envelope_demo.js
@@ -1,12 +1,19 @@
 window.onload = function(){
 
 var audioCtx = new window.AudioContext();
+
 var oscElement = document.querySelector('.osc');
 var oscillator = new Osc(oscElement, audioCtx);
+
 var ampElement = document.querySelector('.amp');
 var amp = new Amp(ampElement, audioCtx);
+
+var scopeElement = document.querySelector('.bigScope');
+var scope = new Scope(scopeElement, audioCtx);
+
 oscillator.connect(amp.input);
-amp.connect(audioCtx.destination);
+amp.connect(scope.input);
+scope.connect(audioCtx.destination);
 
 var midiHandler = new MidiHandler();
 midiHandler.onnoteon = (frequency => {
